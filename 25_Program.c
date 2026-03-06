766cdcf1957cb28a490c45b27669cc85d96f269e
AniketDarade <86124590+AniketDarade2354@users.noreply.github.com>
2026-03-06 18:36:44 +0530

    Add files via upload

---
+++ 25_Program.c
@@ -0,0 +1,12 @@
+//Sign of the result of division operator
+#include <stdio.h>
+
+int main() 
+{
+    printf("Sign of the result of division operator : \n");
+
+    printf("4/3 = %d,\t -4/3 = %d,\n",4/3, -4/3);
+    printf("4/-3 = %d,\t -4/-3 = %d,\n",4/-3, -4/-3);
+    
+    return 0;
+}
