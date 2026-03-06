73250eb199db5dd1d8ead0e6687cdb7c77c1a1d7
AniketDarade <86124590+AniketDarade2354@users.noreply.github.com>
2026-03-06 18:45:51 +0530

    Add files via upload

---
+++ 27_Program.c
@@ -0,0 +1,12 @@
+//Sign of the result of modulus operator
+#include <stdio.h>
+
+int main() 
+{
+    printf("Sign of the result of modulus operator : \n");
+
+    printf("4%%3 = %d,\t -4%%3 = %d,\n",4%3, -4%3);
+    printf("4%%-3 = %d,\t -4%%-3 = %d,\n",4%-3, -4%-3);
+    
+    return 0;
+}
