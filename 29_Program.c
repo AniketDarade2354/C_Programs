808ede120cd6e192cbe8bf4e14305976350ebdd2
AniketDarade <86124590+AniketDarade2354@users.noreply.github.com>
2026-03-07 23:39:32 +0530

    Add files via upload

---
+++ 29_Program.c
@@ -0,0 +1,14 @@
+//Logical AND operator
+#include <stdio.h>
+
+int main() 
+{
+    int i = 0, j = 1,k = 2, l;
+
+    l = i&&j++&&k++;    // l = (i&&j++)&&k++
+
+    printf("Resultant values after evaluation are:\n");
+    printf("%d %d %d %d\n",i,j,k,l);
+    
+    return 0;
+}
